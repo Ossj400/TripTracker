fac7f3901885d7b596130ccd8deec580543720e9
Artur Osmanowski <ossj400@gmail.com>
2020-01-10 19:15:22 +0100

    UI / security.cs

--- TripTracker.UI/Startup.cs
+++ TripTracker.UI/Startup.cs
@@ -13,68 +13,75 @@ using TripTracker.UI.Data;
 using TripTracker.UI.Services;
 using System.Net.Http;
 
+
 namespace TripTracker.UI
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<ApplicationDbContext>(options =>
+					options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-            #region API Client Configuration
+			services.AddIdentity<ApplicationUser, IdentityRole>()
+					.AddEntityFrameworkStores<ApplicationDbContext>()
+					.AddDefaultTokenProviders();
 
-            services.AddScoped(_=> new HttpClient
-            {
-                BaseAddress = new Uri(Configuration["serviceUrl"])
-            });
-            services.AddScoped<IApiClient, ApiClient>();
+			#region API Client Configuration
 
-            #endregion
+			services.AddScoped(_ =>
+				new HttpClient
+				{
+					BaseAddress = new Uri(Configuration["serviceUrl"])
+				});
+			services.AddScoped<IApiClient, ApiClient>();
 
+			#endregion
 
-            services.AddMvc()
-                .AddRazorPagesOptions(options =>
-                {
-                    options.Conventions.AuthorizeFolder("/Account/Manage");
-                    options.Conventions.AuthorizePage("/Account/Logout");
-                });
+			services.AddMvc()
+					.AddRazorPagesOptions(Security.Configure);
 
-            // Register no-op EmailSender used by account confirmation and password reset during development
-            // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=532713
-            services.AddSingleton<IEmailSender, EmailSender>();
-        }
+			services.AddAuthorization(configure =>
+			{
+				configure.AddPolicy("CreateTrips", policy =>
+				{
+					policy.RequireUserName("Artur@Osmanowski.pl")
+						.Build();
+				});
+			});
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
+			// Register no-op EmailSender used by account confirmation and password reset during development
+			// For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=532713
+			services.AddSingleton<IEmailSender, EmailSender>();
+		}
 
-            app.UseStaticFiles();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseBrowserLink();
+				app.UseDeveloperExceptionPage();
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+			}
 
-            app.UseAuthentication();
+			app.UseStaticFiles();
 
-            app.UseMvc();
-        }
-    }
+			app.UseAuthentication();
+
+			app.UseMvc();
+		}
+	}
 }
